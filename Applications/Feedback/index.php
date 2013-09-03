9fd88c601faedd526f513313ecf544243c536339
Vamshedhar Reddy <vamshedhar@gmail.com>
2013-09-03 13:38:33 -0700

    Feedback almost complete.
    Signed-off-by: Vamshedhar Reddy <vamshedhar@gmail.com>

--- Applications/Feedback/index.php
+++ Applications/Feedback/index.php
@@ -1,11 +1,11 @@
 <?php
-require_once "$_SERVER[DOCUMENT_ROOT]/PHP_Code/__autoload.php";
 /*
  *
 *@author Vamshedhar Reddy C
 *Date Created: 01/09/13
 *
 */
+require_once "$_SERVER[DOCUMENT_ROOT]/PHP_Code/__autoload.php";
 ThisPage::renderTop(Feedback);
 function ref_clean($subject){
 	$pattern = "/\.php$|index\.php$/";
@@ -15,19 +15,57 @@ function ref_clean($subject){
 $ref_array = array_filter(array_map("ref_clean", preg_split('(\/|\\\\)',$_GET['ref'])));
 $ref_string = implode(" &rsaquo; ", $ref_array);s
 ?>
-<h1>Feedback</h1>
-<h4>Feedback for  <?php  echo $ref_string;?></h4>
-<form action="thankyou.php" method="post">
-<table>
-<tr><td>Type:
-<select name="type">
-	<option value="suggestion">Suggestion</option>
-	<option value="bug">Bug / Complaint</option>
-</select></td></tr>
-<tr><td>Description:<textarea name="description" rows="6" cols="100" style="resize: none;" placeholder="Your Feedback goes here..."></textarea></td></tr>
-<tr><td><input type="submit" value="Submit" name="submit"></td></tr>
-</table>
-</form>
+<script type="text/javascript">
+$(document).ready(function(){
+	var feedback=false;
+	function checkall(){
+		var submitbutton=$("#submitFeedback");
+		if(feedback==true){
+			submitbutton.prop('disabled',false);
+		}
+		else{
+			submitbutton.prop('disabled',true);
+		}
+	}
+	var description=$("#description");
+	description.keyup(function(){
+				if(description.val().length > 0){
+					feedback = true;
+				} else {
+					feedback = false;
+				}
+				checkall();
+	});
+});
+</script>
+<?php 
+	if (isset($_GET['ref'])) {
+		?>
+		<h1>Feedback</h1>
+		<h4>Feedback for  <?php  echo $ref_string;?></h4>
+		<form action="thankyou.php" method="post">
+		<table>
+		<tr><td>Type:
+		<select name="type">
+			<option value="suggestion">Suggestion</option>
+			<option value="bug">Bug / Complaint</option>
+		</select></td></tr>
+		<tr>
+			<td>
+				Description:
+				<textarea name="description" id="description" rows="6" cols="100" style="resize: none;" placeholder="Your Feedback goes here..."></textarea>
+				<input type="hidden" name="ref" value="<?php echo $_GET['ref']?>" />
+			</td>
+		</tr>
+		<tr><td><input type="submit" value="Submit" name="submit" id="submitFeedback"  disabled=disabled></td></tr>
+		</table>
+		</form>
+		<?php
+	} else {
+		header("Location: ../Home/");
+	}
+?>
+
 <?php 
 ThisPage::renderBottom();
 ?>
--- Applications/Feedback/thankyou.php
+++ Applications/Feedback/thankyou.php
@@ -1,8 +1,21 @@
 <?php
 require_once "$_SERVER[DOCUMENT_ROOT]/PHP_Code/__autoload.php";
 ThisPage::renderTop("Thank You");
-?>
-<h1>Thank You :)</h1>
-<?php 
+if (isset($_POST['submit'])) {
+	$mysql=MySQL::getConnection();
+	$description=$mysql->real_escape_string($_POST["description"]);
+	$ref=$mysql->real_escape_string($_POST["ref"]);
+	$type = $mysql->real_escape_string($_POST["type"]);
+	if ($mysql->query("INSERT INTO feedback SET reference='$ref', type='$type', description='$description', user_id=''")) {
+		?>
+		<h1>Thank You :)</h1><br>
+		<div id="thankyou" class="success">Thank You for your valuable feeback. We will get back to you soon.<br><br>- Regards IMG.</div>
+		<?php 
+	} else {
+		echo "Query failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+} else {
+	header("Location: ../Home/");
+} 
 ThisPage::renderBottom();
 ?>
