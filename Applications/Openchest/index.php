0ce126027824b2a348c14720b1258cbefb664a4f
Abhas Mittal <mittalabhas1@hotmail.com>
2013-09-04 02:05:49 +0530

    Signed-off-by: Abhas Mittal <mittalabhas1@hotmail.com>

--- Applications/Openchest/index.php
+++ Applications/Openchest/index.php
@@ -6,19 +6,19 @@ require_once "$_SERVER[DOCUMENT_ROOT]/PHP_Code/__autoload.php";
 ThisPage::renderTop("OPENCHEST");
 ?>
 <h1>OPENCHEST</h1>
-<form action="?path=<?php echo $_GET['path']; ?>" method="post" enctype="multipart/form-data">
+<form action="?ref=<?php echo $_GET['ref']; ?>" method="post" enctype="multipart/form-data">
 <fieldset><legend>Upload File</legend>
 Filename: <input type="file" name="file" id="file" />
-<input type="hidden" name="path" value="<?php echo $_GET['path']; ?>" />
+<input type="hidden" name="path" value="<?php echo $_GET['ref']; ?>" />
 <input type="submit" name="fileSubmit" value="Upload File" />
 </fieldset>
 </form>
 
 <br />
-<form action="?path=<?php echo $_GET['path']; ?>" method="post" enctype="multipart/form-data">
+<form action="?ref=<?php echo $_GET['ref']; ?>" method="post" enctype="multipart/form-data">
 <fieldset><legend>Create Folder</legend>
-Make Folder: <input type="text" name="folder" />
-<input type="hidden" name="path" value="<?php echo $_GET['path']; ?>" />
+<label for="folder">Folder Name: </label><input type="text" name="folder" />
+<input type="hidden" name="path" value="<?php echo $_GET['ref']; ?>" />
 <input type="submit" name="folderSubmit" value="Make Folder" />
 </fieldset>
 </form>
